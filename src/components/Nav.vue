490c3c561de6e3e03cb9ea05c3f2865e105893c8
Deya BERGER <dberger@e1r9p3.clusters.42paris.fr>
2022-09-23 17:58:00 +0200

    avec good navbar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -8,21 +8,35 @@
 
 
 <template>
-	<b-navbar id = "nav" toggleable type="dark" variant="dark">
-		<b-navbar-brand :to="{ path: '/' }">Hypertube</b-navbar-brand>
-			<b-nav-item v-if="!user_connected" :to="{ path: '/sign_in'}">Sign in</b-nav-item>
-			<b-nav-item v-if="!user_connected" :to="{ path: '/'}">Sign up</b-nav-item>
-			
-			<b-nav-item v-if="user_connected" :to="{ path: '/'}">Search</b-nav-item>
-			<b-nav-item v-if="user_connected" :to="{ path: '/'}">Profile</b-nav-item>
-			<b-nav-item v-if="user_connected" :to="{ path: '/'}">Exit</b-nav-item>
-	</b-navbar>
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <routerLink to="/" class="navbar-brand">Hypertube</routerLink>
+    <ul class="navbar-nav ms-auto" v-if = "!user_connected">
+        <li class="nav-item">
+			<router-link to="/sign_in" class="nav-link">Sign in</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link to="/" class="nav-link" >Sign up</router-link>
+        </li>
+    </ul>
+	<ul class="navbar-nav ms-auto" v-if = "user_connected">
+        <li class="nav-item">
+            <router-link to="/" class="nav-link">Search</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link to="/" class="nav-link">Profile</router-link>
+        </li>
+		<li class="nav-item">
+            <router-link to="/" class="nav-link">Exit</router-link>
+        </li>
+    </ul>
+</nav>
+
 </template>
 
 <style scoped >
 
-.nav-item{
+/* .nav-item{
 	list-style-type: none;
-}
+} */
 
 </style>
