d692db1c50c0439108f5af413848ce5ac7565d60
Deya Berger <bergerdeya@gmail.com>
2022-09-26 17:12:36 +0200

    avec login buttons

--- src/views/Sign_in.vue
+++ src/views/Sign_in.vue
@@ -63,13 +63,24 @@ export default {
       <div class="col-md-12 text-center" :class="{ 'mt-4' : connection_error, 'mt-5' : !connection_error }">
       <p class="error_msg" v-show="connection_error">Wrong username or password</p>
       <button class="submit_button" type="submit">Sign in</button>
+      <div class = "mt-3">OR</div>
       </div>
       <button class="mt-3 loginBtn loginBtn--facebook">
         Login with Facebook
       </button>
-      <button class="mt-3 loginBtn loginBtn--google">
+      <span>
+      <button class="loginBtn loginBtn--google">
         Login with Google
       </button>
+    </span>
+    <button class="mt-3 loginBtn loginBtn--42">
+        Login with 42
+      </button>
+      <span>
+        <button class="mt-3 loginBtn loginBtn--twitter">
+        Login with twitter
+      </button>
+      </span>
       <div class="change_page mt-3 text-center">
       <router-link to="/reset_pwd">Forgot your password?</router-link>
     </div>
@@ -78,11 +89,13 @@ export default {
     </div>
     </form>
    </div>
+
 </template>
 
 
 <style >
 @import "../assets/shared_scss/login.scss";
+
 
 .loginBtn {
   box-sizing: border-box;
@@ -91,9 +104,8 @@ export default {
   border: none;
   text-align: left;
   line-height: 34px;
-  font-size: 16px;
+  font-size: 13px;
   color: #FFF;
-  display: inline-block;
 }
 
 .loginBtn:before {
@@ -120,7 +132,7 @@ export default {
   background-image: linear-gradient(#4C69BA, #3B55A0);
   /*font-family: "Helvetica neue", Helvetica Neue, Helvetica, Arial, sans-serif;*/
   text-shadow: 0 -1px 0 #354C8C;
-  width: 57%;
+  width: 49%;
 }
 
 .loginBtn--facebook:before {
@@ -136,16 +148,65 @@ export default {
 /* Google */
 .loginBtn--google {
   /*font-family: "Roboto", Roboto, arial, sans-serif;*/
-  background: #DD4B39;
-  width: 57%;
+  background: #fef8f7;
+  color: black;
+  width: 45%;
+  margin-left: 5%;
+
 }
 .loginBtn--google:before {
-  border-right: #BB3F30 1px solid;
-  background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_google.png') 6px 6px no-repeat;
+  border-right: #c0bcbc 1px solid;
+  background: url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4=') 6px 6px no-repeat;
 }
 .loginBtn--google:hover,
-.loginBtn--google:focus {
-  background: #E74B37;
+.loginBtn--42:hover,
+.loginBtn--google:focus,
+.loginBtn--42:focus {
+  background-color: #f3eceb;
+  background-image: linear-gradient(#e3e5e5, #c7c7c7e4);
 }
+
+
+/* 42 */
+
+.loginBtn--42 {
+  /*font-family: "Roboto", Roboto, arial, sans-serif;*/
+  background: #fef8f7;
+  color: black;
+  width: 49%;
+}
+.loginBtn--42:before {
+  border-right: #c0bcbc 1px solid;
+  background : url('https://upload.wikimedia.org/wikipedia/commons/8/8d/42_Logo.svg?uselang=fr');
+  background-size: 70%;
+  background-repeat: no-repeat;
+  background-position: 50%;
+}
+
+/* Twitter */
+
+.loginBtn--twitter {
+  /*font-family: "Roboto", Roboto, arial, sans-serif;*/
+  color: white;
+  background-image: linear-gradient(#14a3dcf1, #006188);
+  width: 45%;
+  margin-left: 5%;
+
+}
+.loginBtn--twitter:before {
+  border-right: #c0bcbc 1px solid;
+  background : url('../assets/twitter.svg');
+  background-size: 70%;
+  background-repeat: no-repeat;
+  background-position: 50%;
+}
+
+
+.loginBtn--twitter:hover,
+.loginBtn--twitter:focus {
+  background-image: linear-gradient(#47b1dc, #2285ac);
+}
+
+
 
 </style>
