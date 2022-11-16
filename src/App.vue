11f2edf90c45cce474805106f0f66945d9b83708
Deya BERGER <dberger@e3r11p8.clusters.42paris.fr>
2022-11-16 18:51:06 +0100

    on avance sur le background de l'image

--- src/App.vue
+++ src/App.vue
@@ -25,21 +25,19 @@ export default {
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap");
 
-:root {
-	--font: Roboto, sans-serif;
-	--textColor: #f6f8fc;
+
+#app {
+	font-family: Roboto, sans-serif;
+	color: #f6f8fc;
 	background-color: black;
 	background-image: linear-gradient(to bottom, transparent 90%, black, black), url("/src/assets/background2.jpg");
 	background-size: cover;
 	background-repeat: no-repeat;
 	background-position: center;
-	height: 100%;
+	height: 100vh;
 	width: 100%;
 }
 
-#app {
-	font-family: var(--font);
-	color: var(--textColor);
-}
+
 
 </style>
--- src/views/Single_movie.vue
+++ src/views/Single_movie.vue
@@ -1,12 +1,17 @@
 <script>
 	import { ref, computed } from 'vue'
 	import { mapState, useStore } from 'vuex';
+	import fakeData from "./fake_data.json";
 
 	export default {
+		props: {
+			moviename: String
+		},
 		data() {
-		const store = useStore()
-		return {
-		}
+			const store = useStore()
+			return {
+				movie: fakeData
+			}
 		},
 		computed: mapState({
 		language: state =>  state.language,
@@ -20,5 +25,97 @@
 </script>
 
 <template>
-	<div>HEY </div>
+	<b-container fluid="sm">
+		<div class="row">
+			<div class="col video">
+				THIS IS THE VIDEO {{moviename}}
+			</div>
+		</div>
+		<div class="row movie_name">
+			<div class="col">
+				THIS IS THE MOVIE NAME
+			</div>
+		</div>
+		<div class="row general_infos">
+			<div class="col type">
+				TYPE
+			</div>
+			<div class="col year">
+				YEAR {{movie.year}}
+			</div>
+			<div class="col duration">
+				DURATION
+			</div>
+			<div class="col score">
+				SCORE
+			</div>
+		</div>
+		<div class="row summary">
+			<div class="col">
+				THIS IS THE LOOOOOOOOOOOOONG SUMMARY
+			</div>
+		</div>
+		<div class="row director">
+			<div class="col">
+				THIS IS THE DIRECTOR NAME
+			</div>
+		</div>
+		<div class="row actors">
+			<div class="col">
+				THIS IS THE ACTORS NAMES
+			</div>
+		</div>
+		100% wide until small breakpoint
+	</b-container>
 </template>
+
+
+<style>
+
+#app {
+	background:linear-gradient(
+      rgba(30,27,38, 0.95), 
+      rgba(30,27,38, 0.95)),
+    url("https://i.ibb.co/FDGqCmM/papers-co-ag74-interstellar-wide-space-film-movie-art-33-iphone6-wallpaper.jpg");
+  background-position: center;
+  background-size: cover;
+  background-repeat: repeat;
+}
+
+
+</style>
+
+<style lang="scss" scoped>
+
+.container-sm {
+	background-color: rgb(238, 255, 0);
+}
+
+.video {
+	background-color: green;
+	height: 300px;
+}
+
+.movie_name {
+	background-color: red;
+}
+
+
+.general_infos {
+	background-color: blue;
+}
+
+.summary {
+	background-color: pink;
+	height: 200px;
+}
+
+.director {
+	background-color: rgb(192, 255, 254);
+}
+
+.actors {
+	background-color: rgb(39, 225, 25);
+}
+
+</style>
