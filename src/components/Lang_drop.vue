2d5d42ebc76d7699dbf340d447072e1fafaa6f11
Deya BERGER <dberger@e3r10p6.clusters.42paris.fr>
2022-11-18 14:40:03 +0100

    avec nouveau component

---
+++ src/components/Lang_drop.vue
@@ -0,0 +1,35 @@
+<script>
+import { ref, computed } from 'vue'
+import { mapState, useStore } from 'vuex';
+import textContent from "../assets/language_dict/language_dict.json"
+
+export default {
+	data() {
+      const store = useStore()
+      return {
+      }
+    },
+	computed : mapState({
+      lang_nb         : state =>  state.lang_nb,
+      language        : state =>  state.language,
+      language_options: state =>  state.language_options,
+    }),
+	methods: {
+		change_language(new_language) {
+			console.log("Changing language to: " + new_language)
+			this.$store.commit('SET_LANGUAGE', new_language)
+		},
+	}
+}
+</script>
+
+<template>
+	<b-nav-item>
+		<b-dropdown variant="link" class="dropdown_menu">
+			<template #button-content>
+				<b-icon-gear-fill/>
+			</template>
+			<b-dropdown-item :class="option==language ? 'dropdown_item disabled' : 'dropdown_item'" v-for="option in language_options" :key="option" @click="change_language(option)" :disabled="option==language ? true : false">{{option}}</b-dropdown-item>
+		</b-dropdown>
+	</b-nav-item>
+</template>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,8 +2,10 @@
 import { ref, computed } from 'vue'
 import { mapState, useStore } from 'vuex';
 import textContent from "../assets/language_dict/language_dict.json"
+import LangDrop from '../components/Lang_drop.vue';
 
 export default {
+  components: { LangDrop },
 	data() {
       const store = useStore()
       return {
@@ -13,15 +15,7 @@ export default {
 	computed: mapState({
       user_connected  : state =>  state.user_connected,
       lang_nb         : state =>  state.lang_nb,
-      language         : state =>  state.language,
-      language_options: state =>  state.language_options,
-    }),
-	methods: {
-		change_language(new_language) {
-			console.log("Changing language to: " + new_language)
-			this.$store.commit('SET_LANGUAGE', new_language)
-		},
-	}
+    })
 }
 </script>
 
@@ -38,14 +32,7 @@ export default {
 		<b-nav-item>
 			<router-link to="/sign_up" class="nav-link">{{text_content.sign_up[lang_nb]}}</router-link>
 		</b-nav-item>
-		<b-nav-item>
-			<b-dropdown variant="link" class="dropdown_menu">
-				<template #button-content>
-					<b-icon-gear-fill/>
-				</template>
-				<b-dropdown-item :class="option==language ? 'dropdown_item disabled' : 'dropdown_item'" v-for="option in language_options" :key="option" @click="change_language(option)" :disabled="option==language ? true : false">{{option}}</b-dropdown-item>
-			</b-dropdown>
-		</b-nav-item>
+		<LangDrop></LangDrop>
       </b-navbar-nav>
 
       <b-navbar-nav class="ms-auto" v-else>
@@ -55,14 +42,7 @@ export default {
         <b-nav-item>
 			<router-link to="/profile" class="nav-link">{{text_content.profile[lang_nb]}} <b-icon-person-circle/></router-link>
 		</b-nav-item>
-		<b-nav-item>
-			<b-dropdown variant="link" class="dropdown_menu">
-				<template #button-content>
-					<b-icon-gear-fill/>
-				</template>
-				<b-dropdown-item :class="option==language ? 'dropdown_item disabled' : 'dropdown_item'" v-for="option in language_options" :key="option" @click="change_language(option)" :disabled="option==language ? true : false">{{option}}</b-dropdown-item>
-			</b-dropdown>
-		</b-nav-item>
+		<LangDrop></LangDrop>
 		<b-nav-item>
 			<router-link to="/" class="nav-link">{{text_content.logout[lang_nb]}} <b-icon-arrow-bar-right /></router-link>
 		</b-nav-item>
@@ -73,10 +53,5 @@ export default {
 
 <style scoped lang="scss">
 @import url("./../assets/shared_scss/navbars.scss");
-
-</style>
-
-<style scoped lang="css">
-
 
 </style>
