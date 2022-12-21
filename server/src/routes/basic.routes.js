a87c03a09415ebdeaba0d33afeda0c52f96a9a7a
Joep B <joepbarmentlo@gmail.com>
2022-12-21 16:52:59 +0100

    most routes work

--- server/src/routes/basic.routes.js
+++ server/src/routes/basic.routes.js
@@ -4,10 +4,14 @@ const placeholder = async (req, res) => {
 }
 
 module.exports = (db_pool) => {
-    const user_controller  = require("../controllers/user.controller" )(db_pool)
-    const movie_controller = require("../controllers/movie.controller")(db_pool)
+    const user_controller    = require("../controllers/user.controller"   )(db_pool)
+    const movie_controller   = require("../controllers/movie.controller"  )(db_pool)
+    const comment_controller = require("../controllers/comment.controller")(db_pool)
+
     console.log(user_controller)
     console.log(movie_controller)
+    console.log(comment_controller)
+
     var router = require("express").Router();
 
     router.post  ("oauth/token"                , placeholder)
@@ -16,10 +20,10 @@ module.exports = (db_pool) => {
     router.patch ("/users/:id"                 , user_controller.patch_user)
     router.get   ("/movies"                    , movie_controller.get_movies)
     router.get   ("/movies/:id"                , movie_controller.get_movie_by_id)
-    router.get   ("/comments"                  , placeholder)
-    router.get   ("/comments/:id"              , placeholder)
-    router.patch ("/comments/:id"              , placeholder)
-    router.delete("/comments/:id"              , placeholder)
+    router.get   ("/comments"                  , comment_controller.get_comments)
+    router.get   ("/comments/:id"              , comment_controller.get_comment_by_id)
+    router.patch ("/comments/:id"              , comment_controller.patch_comment)
+    router.delete("/comments/:id"              , comment_controller.delete_comment_by_id)
     router.post  ("/comments"                  , placeholder)
     router.post  ("/movies/:movie_id/comments" , placeholder)
 
