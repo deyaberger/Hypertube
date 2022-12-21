6565857ee1edb269fd27e4bf56ff16aee0c5cb79
Joep B <joepbarmentlo@gmail.com>
2022-12-21 12:50:34 +0100

    basic back

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express    = require('express')
+const bodyParser = require('body-parser')
+
+require('dotenv').config()
+
+const connection_pool = require('./src/db/create_connection_pool')
+const router = require("./src/routes/basic.routes")
+const app = express();
+
+const basic_router = require("./src/routes/basic.routes")(connection_pool)
+app.use("/api", basic_router, function(req, res, next){
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  })
+
+const PORT = process.env.PORT || 8070;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -9,6 +9,19 @@ module.exports = (db_pool) => {
                 res.status(200).send({data: users})
             }
             catch (e) {
+                res.status(500).send()
+                throw(e)
+            }
+        },
+
+        get_users : async(req, res) => {
+            try {
+                let [users, ] = await db_pool.query(
+                    "SELECT username, mail, picture FROM users"
+                )
+                res.status(200).send({data: users})
+            }
+            catch (e) {
                 throw(e)
             }
         }
