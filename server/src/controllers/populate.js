79e5a25e149d3aa0e864556c0bc5e0ab1bea1996
Joep B <joepbarmentlo@gmail.com>
2023-02-26 14:21:36 +0000

    foldable

--- server/src/controllers/populate.js
+++ server/src/controllers/populate.js
@@ -6,214 +6,219 @@ const bar1              = new cliProgress.SingleBar({}, cliProgress.Presets.shad
 
 const API_KEY = process.env.TMDB_API_KEY
 
-function create_request(url, params) {
-	let request = {
-		url: url,
-		method: "get",
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-			"Content-type"               : "application/json",
-		},
-		params: params
-	}
-	return request;
-}
-
-function transform_cast_dict(cast, crew) {
-    let cast_output = cast.reduce((acc, { known_for_department, original_name, popularity}) => {
-        if (known_for_department === "Directing") {
-          acc.director.push({ original_name: original_name, popularity});
-        } else if (known_for_department === "Acting") {
-          acc.actors.push({ original_name: original_name, popularity});
-        }
-        return acc;
-      }, { director: [], actors: [] });
-      if (cast_output.director.length == 0 && crew != null && crew.length != 0) {
-        cast_output.director = crew.reduce((acc, { known_for_department, original_name, popularity}) => {
-            if (known_for_department === "Directing") {
-              acc.push({ original_name: original_name, popularity});}
-            return acc;
-          }, []);
-      }
-      const topActors = cast_output.actors.sort((a, b) => b.popularity - a.popularity).slice(0, 3);
-      const topActorNames = topActors.map(actor => actor.original_name);
-      const topDirectors = cast_output.director.sort((a, b) => b.popularity - a.popularity).slice(0, 1);
-      const topDirectorName = topDirectors.map(actor => actor.original_name);
-      cast_output.actors = topActorNames
-      cast_output.director = topDirectorName
-      return cast_output
-}
-
-async function read_json(file_path) {
-  try {
-    const data = await fs.readFile(file_path, { encoding: 'utf8' });
-    return data
-  } catch (err) {
-    console.log("Cant read file: ", file_path);
-    console.log(err);
-    return null;
-  }
-}
-
-function parse_movie_data(movie) {
-    let yts_id         = movie.hasOwnProperty('id')        ? movie.id        : null;
-    let imdb_code      = movie.hasOwnProperty('imdb_code') ? movie.imdb_code : null;
-    let title          = movie.hasOwnProperty('title')     ? movie.title     : null;
-    let imdb_rating    = movie.hasOwnProperty('rating')    ? movie.rating    : null;
-    let year           = movie.hasOwnProperty('year')      ? movie.year      : null;
-    let length_minutes = movie.hasOwnProperty('runtime')   ? movie.runtime   : null;
-    let language       = movie.hasOwnProperty('language')  ? movie.language  : null;
-    let summary        = movie.hasOwnProperty('summary')   ? movie.summary   : null;
-    if (summary.length > 4000) {
-        console.log("Summary too long, using synopsis for movie: ", movie.title)
-        summary = movie.hasOwnProperty('synopsis')   ? movie.synopsis   : null;
-        if (summary.length > 4000) {
-            console.log("Slicing synopsis for movie: ", movie.title)
-            summary = summary.slice(0, 3999)
-        }
-    }
-    if (summary.length == 0) {
-        summary = 'No summary provided';
-    }
-    return ({
-        "yts_id"         : yts_id,
-        "imdb_code"      : imdb_code,
-        "title"          : title,
-        "imdb_rating"    : imdb_rating,
-        "year"           : year,
-        "length_minutes" : length_minutes,
-        "language"       : language,
-        "summary"        : summary
-    })
-}
-
-async function post_movie(db_pool, yts_id, imdb_code, title, imdb_rating, year, length_minutes, language, summary) {
-
-    try {
-        [movie, ] = await db_pool.query("\
-        INSERT INTO movies (yts_id,   imdb_code,   title,   imdb_rating,   year,   length_minutes,   language,   summary) \
-        VALUES             (?,        ?,           ?,       ?,             ?,      ?,                ?,          ?      );",
-                           [yts_id, imdb_code, title, imdb_rating, year, length_minutes, language, summary])
-    }
-    catch (e) {
-        if (e.code == 'ER_DUP_ENTRY') {
-            return ({"msg" : "duplicate"});
-        }
-        if (e.code == 'ER_DATA_TOO_LONG') {
-            console.log("DATA TOO LONG for movie: ", m.title);
-            console.log("Summary length: ", m.summary.length)
-            console.log(e);
-            return(null);
-        }
-        console.log(e);
-        return (null);
-    }
-    return (movie.insertId)
-
-}
-
-async function post_image(db_pool, movie_id, size, url) {
-
-    try {
-        [image, ] = await db_pool.query("\
-        INSERT INTO images (movie_id, size, url) \
-        VALUES             (?,        ?,    ?  );",
-                           [movie_id, size,  url])
-    }
-    catch (e) {
-        throw(e)
-    }
-}
-
-async function parse_and_post_images(db_pool, movie, movie_id) {
-    let promises = []
-    if (movie.hasOwnProperty('large_cover_image')) {
-        promises.push(post_image(db_pool, movie_id, 1, movie.large_cover_image))
-    }
-    if (movie.hasOwnProperty('medium_cover_image')) {
-        promises.push(post_image(db_pool, movie_id, 2, movie.medium_cover_image))
-    }
-    if (movie.hasOwnProperty('small_cover_image')) {
-        promises.push(post_image(db_pool, movie_id, 3, movie.small_cover_image))
-    }
-    if (movie.hasOwnProperty('background_image_original')) {
-        promises.push(post_image(db_pool, movie_id, 4, movie.background_image_original))
-    }
-    if (movie.hasOwnProperty('background_image')) {
-        promises.push(post_image(db_pool, movie_id, 5, movie.background_image))
-    }
-    await Promise.all(promises)
-}
-
-async function post_genre(db_pool, movie_id, name) {
-
-    try {
-        [genre, ] = await db_pool.query("\
-        INSERT INTO genres (movie_id, name) \
-        VALUES             (?,        ?);",
-                           [movie_id, name])
-    }
-    catch (e) {
-        throw(e)
-    }
-}
-
-async function parse_and_post_genres(db_pool, movie, movie_id) {
-    if (!movie.hasOwnProperty('genres')) {
-        return ({"msg" : "missing_genres"});
-    }
-    let genres = movie.genres;
-    if (typeof(genres) != "object" || genres.length == 0) {
-        return ({"msg" : "missing_genres"});
-    }
-    let promises = []
-    for (let i in genres) {
-        promises.push(post_genre(db_pool, movie_id, genres[i]))
-    }
-    await Promise.all(promises)
-    return ({"msg" : "genres_all_good"})
-}
-
-async function post_torrent(db_pool, movie_id, url, hash, quality, seeds, peers, size, size_bytes) {
-
-    try {
-        [torrent, ] = await db_pool.query("\
-        INSERT INTO torrents (movie_id, url, hash, quality, seeds, peers, size, size_bytes) \
-        VALUES               (?,        ?,   ?,    ?,       ?,     ?,     ?,    ?         );",
-                             [movie_id, url, hash, quality, seeds, peers, size, size_bytes])
-    }
-    catch (e) {
-        throw(e)
-    }
-}
-
-async function parse_and_post_torrents(db_pool, movie, movie_id) {
-    if (!movie.hasOwnProperty('torrents')) {
-        return ({"msg" : "missing_torrents"});
-    }
-    let torrents = movie.torrents;
-    if (typeof(torrents) != "object" || torrents.length == 0) {
-        return ({"msg" : "missing_torrents"});
-    }
-    let torrents_promises = []
-    for (let i in torrents) {
-        let torrent = torrents[i];
-        let url = torrent.hasOwnProperty('url')               ? torrent.url               : null;
-        let hash = torrent.hasOwnProperty('hash')             ? torrent.hash              : null;
-        let quality = torrent.hasOwnProperty('quality')       ? parseInt(torrent.quality) : null;
-        let seeds = torrent.hasOwnProperty('seeds')           ? torrent.seeds             : null;
-        let peers = torrent.hasOwnProperty('peers')           ? torrent.peers             : null;
-        let size = torrent.hasOwnProperty('size')             ? torrent.size              : null;
-        let size_bytes = torrent.hasOwnProperty('size_bytes') ? String(torrent.size_bytes): null;
-        torrents_promises.push(post_torrent(db_pool, movie_id, url, hash, quality, seeds, peers, size, size_bytes))
-    }
-    Promise.all(torrents_promises)
-    return ({"msg" : "torrents_all_good"})
-}
-
 module.exports = (db_pool) => {
     return {
 
+        create_request: (url, params) => {
+            let request = {
+                url: url,
+                method: "get",
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    "Content-type"               : "application/json",
+                },
+                params: params
+            }
+            return request;
+        },
+        
+        transform_cast_dict: (cast, crew) => {
+            let cast_output = cast.reduce((acc, { known_for_department, original_name, popularity}) => {
+                if (known_for_department === "Directing") {
+                  acc.director.push({ original_name: original_name, popularity});
+                } else if (known_for_department === "Acting") {
+                  acc.actors.push({ original_name: original_name, popularity});
+                }
+                return acc;
+              }, { director: [], actors: [] });
+              if (cast_output.director.length == 0 && crew != null && crew.length != 0) {
+                cast_output.director = crew.reduce((acc, { known_for_department, original_name, popularity}) => {
+                    if (known_for_department === "Directing") {
+                      acc.push({ original_name: original_name, popularity});}
+                    return acc;
+                  }, []);
+              }
+              const topActors = cast_output.actors.sort((a, b) => b.popularity - a.popularity).slice(0, 3);
+              const topActorNames = topActors.map(actor => actor.original_name);
+              const topDirectors = cast_output.director.sort((a, b) => b.popularity - a.popularity).slice(0, 1);
+              const topDirectorName = topDirectors.map(actor => actor.original_name);
+              cast_output.actors = topActorNames
+              cast_output.director = topDirectorName
+              return cast_output
+        },
+        
+        read_json: async (file_path) => {
+          try {
+            const data = await fs.readFile(file_path, { encoding: 'utf8' });
+            return data
+          } catch (err) {
+            console.log("Cant read file: ", file_path);
+            console.log(err);
+            return null;
+          }
+        },
+        
+        parse_movie_data: (movie) => {
+            let yts_id         = movie.hasOwnProperty('id')        ? movie.id        : null;
+            let imdb_code      = movie.hasOwnProperty('imdb_code') ? movie.imdb_code : null;
+            let title          = movie.hasOwnProperty('title')     ? movie.title     : null;
+            let imdb_rating    = movie.hasOwnProperty('rating')    ? movie.rating    : null;
+            let year           = movie.hasOwnProperty('year')      ? movie.year      : null;
+            let length_minutes = movie.hasOwnProperty('runtime')   ? movie.runtime   : null;
+            let language       = movie.hasOwnProperty('language')  ? movie.language  : null;
+            let summary        = movie.hasOwnProperty('summary')   ? movie.summary   : null;
+            if (summary.length > 4000) {
+                console.log("Summary too long, using synopsis for movie: ", movie.title)
+                summary = movie.hasOwnProperty('synopsis')   ? movie.synopsis   : null;
+                if (summary.length > 4000) {
+                    console.log("Slicing synopsis for movie: ", movie.title)
+                    summary = summary.slice(0, 3999)
+                }
+            }
+            if (summary.length == 0) {
+                summary = 'No summary provided';
+            }
+            return ({
+                "yts_id"         : yts_id,
+                "imdb_code"      : imdb_code,
+                "title"          : title,
+                "imdb_rating"    : imdb_rating,
+                "year"           : year,
+                "length_minutes" : length_minutes,
+                "language"       : language,
+                "summary"        : summary
+            })
+        },
+        
+        post_movie: async (db_pool, yts_id, imdb_code, title, imdb_rating, year, length_minutes, language, summary) => {
+        
+            try {
+                [movie, ] = await db_pool.query("\
+                INSERT INTO movies (yts_id,   imdb_code,   title,   imdb_rating,   year,   length_minutes,   language,   summary) \
+                VALUES             (?,        ?,           ?,       ?,             ?,      ?,                ?,          ?      );",
+                                   [yts_id, imdb_code, title, imdb_rating, year, length_minutes, language, summary])
+            }
+            catch (e) {
+                if (e.code == 'ER_DUP_ENTRY') {
+                    return ({"msg" : "duplicate"});
+                }
+                if (e.code == 'ER_DATA_TOO_LONG') {
+                    console.log("DATA TOO LONG for movie: ", m.title);
+                    console.log("Summary length: ", m.summary.length)
+                    console.log(e);
+                    return(null);
+                }
+                console.log(e);
+                return (null);
+            }
+            return (movie.insertId)
+        
+        },
+        
+        post_image: async (db_pool, movie_id, size, url) => {
+        
+            try {
+                [image, ] = await db_pool.query("\
+                INSERT INTO images (movie_id, size, url) \
+                VALUES             (?,        ?,    ?  );",
+                                   [movie_id, size,  url])
+            }
+            catch (e) {
+                throw(e)
+            }
+        },
+        
+        parse_and_post_images: async (db_pool, movie, movie_id) => {
+            let promises = []
+            if (movie.hasOwnProperty('large_cover_image')) {
+                promises.push(post_image(db_pool, movie_id, 1, movie.large_cover_image))
+            }
+            if (movie.hasOwnProperty('medium_cover_image')) {
+                promises.push(post_image(db_pool, movie_id, 2, movie.medium_cover_image))
+            }
+            if (movie.hasOwnProperty('small_cover_image')) {
+                promises.push(post_image(db_pool, movie_id, 3, movie.small_cover_image))
+            }
+            if (movie.hasOwnProperty('background_image_original')) {
+                promises.push(post_image(db_pool, movie_id, 4, movie.background_image_original))
+            }
+            if (movie.hasOwnProperty('background_image')) {
+                promises.push(post_image(db_pool, movie_id, 5, movie.background_image))
+            }
+            await Promise.all(promises)
+        },
+        
+        post_genre: async (db_pool, movie_id, name) => {
+        
+            try {
+                [genre, ] = await db_pool.query("\
+                INSERT INTO genres (movie_id, name) \
+                VALUES             (?,        ?);",
+                                   [movie_id, name])
+            }
+            catch (e) {
+                throw(e)
+            }
+        },
+        
+        parse_and_post_genres: async (db_pool, movie, movie_id) => {
+            if (!movie.hasOwnProperty('genres')) {
+                return ({"msg" : "missing_genres"});
+            }
+            let genres = movie.genres;
+            if (typeof(genres) != "object" || genres.length == 0) {
+                return ({"msg" : "missing_genres"});
+            }
+            let promises = []
+            for (let i in genres) {
+                promises.push(post_genre(db_pool, movie_id, genres[i]))
+            }
+            await Promise.all(promises)
+            return ({"msg" : "genres_all_good"})
+        },
+        
+        post_torrent: async (db_pool, movie_id, url, hash, quality, seeds, peers, size, size_bytes) => {
+        
+            try {
+                [torrent, ] = await db_pool.query("\
+                INSERT INTO torrents (movie_id, url, hash, quality, seeds, peers, size, size_bytes) \
+                VALUES               (?,        ?,   ?,    ?,       ?,     ?,     ?,    ?         );",
+                                     [movie_id, url, hash, quality, seeds, peers, size, size_bytes])
+            }
+            catch (e) {
+                throw(e)
+            }
+        },
+        
+        parse_and_post_torrents: async (db_pool, movie, movie_id) => {
+            if (!movie.hasOwnProperty('torrents')) {
+                return ({"msg" : "missing_torrents"});
+            }
+            let torrents = movie.torrents;
+            if (typeof(torrents) != "object" || torrents.length == 0) {
+                return ({"msg" : "missing_torrents"});
+            }
+            let torrents_promises = []
+            for (let i in torrents) {
+                let torrent = torrents[i];
+                let url = torrent.hasOwnProperty('url')               ? torrent.url               : null;
+                let hash = torrent.hasOwnProperty('hash')             ? torrent.hash              : null;
+                let quality = torrent.hasOwnProperty('quality')       ? parseInt(torrent.quality) : null;
+                let seeds = torrent.hasOwnProperty('seeds')           ? torrent.seeds             : null;
+                let peers = torrent.hasOwnProperty('peers')           ? torrent.peers             : null;
+                let size = torrent.hasOwnProperty('size')             ? torrent.size              : null;
+                let size_bytes = torrent.hasOwnProperty('size_bytes') ? String(torrent.size_bytes): null;
+                torrents_promises.push(post_torrent(db_pool, movie_id, url, hash, quality, seeds, peers, size, size_bytes))
+            }
+            Promise.all(torrents_promises)
+            return ({"msg" : "torrents_all_good"})
+        }
+    }
+}
+
+
+module.exports = (db_pool) => {
+    return {
 		search_all_movies : async (source, page_nb) => {
 			let url = null;
             let response = null;
