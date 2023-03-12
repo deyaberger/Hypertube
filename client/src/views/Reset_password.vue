b28e6bbe0a3c8f80257e801773561e61e66e721b
deyaberger <bergerdeya@gmail.com>
2023-03-12 13:19:36 +0100

    pass reset successful

--- client/src/views/Reset_password.vue
+++ client/src/views/Reset_password.vue
@@ -1,35 +1,86 @@
 <script>
-import { ref, computed } from 'vue'
 import { mapState } from 'vuex';
 import textContent from "../assets/language_dict/language_dict.json"
+import { New_Pass } from "../functions/auth"
 
 
 export default {
+	props : {
+		hash: {
+			type: String,
+			default: ""
+		},
+	},
 	data() {
 		return {
 			visible        : false,
-			username       : '',
-			password       : '',
-			text_content   : textContent.NEWPASS
+			password       : null,
+			text_content   : textContent.NEWPASS,
+			mdp_error	   : false,
+			error		   : false,
+			regex_pwd 	   : /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{6,}$/
 		}
 	},
+
 	computed: mapState({
 		lang_nb: state => state.lang_nb,
 		user_token : state =>  state.user_token,
-		mdp_error: state => state.mdp_error, // TO BE CHAAAAAAAAAAAANGED!!!
 	}),
+
 	methods: {
 		update_password_visibility() {
 			this.visible = !this.visible
 		},
+
+		async onSubmit(e) {
+			console.log("[forgot pwd]: Sendin new password...");
+			e.preventDefault();
+			try {
+				let res = await New_Pass(this.hash, this.password)
+				if (res && res.data && res.data.code == "SUCCESS") {
+					console.log("[forgot pwd]: Reset pwd successfull!")
+					this.$router.push('/sign_in')
+					return true
+				}
+				this.error = true
+				this.mdp_error = false
+				if (res && res.data && res.data.code == "FAILURE") {
+					console.log("[reset pwd]: FAILURE", res.data.msg)
+				}
+			}
+			catch (e) {
+				console.log("[reset pwd]: UNKOWN ERROR in reset pwd!", e)
+				this.error = true
+			}
+			return false
+		},
 	},
+
+	watch : {
+		password: {
+			handler:function() {
+				if (this.password == null || this.password.length == 0 || this.password.match(this.regex_pwd) == null){
+					this.mdp_error = true
+				}
+				else {
+					this.mdp_error = false
+				}
+			},
+			deep:true
+		},
+	},
+
+	mounted() {
+		console.log("[reset_pwd]")
+	}
+
 }
 </script>
 
 
 <template>
 	<div class="container homemade-container ">
-		<form>
+		<form @submit="onSubmit">
 			<h2 class="mb-4 text-center">{{text_content.reset[lang_nb]}}</h2>
 			<div class="input mt-5">
 				<label class = "mb-2" for="password">{{text_content.reset_pwd[lang_nb]}}</label>
@@ -49,10 +100,12 @@ export default {
 						</button>
 					</span>
 				</div>
-				<p class="error_msg" v-show="mdp_error">{{text_content.error[lang_nb]}}</p>
+				<p class="error_msg" v-show="mdp_error">{{text_content.mdp_error[lang_nb]}}</p>
+				<p class="error_msg mt-3" v-show="error">{{text_content.error[lang_nb]}}</p>
 			</div>
 			<div class="col-md-12 text-center mt-5">
-				<button class="submit_button" type="submit">{{text_content.confirm[lang_nb]}}</button>
+				<button v-if="mdp_error" class="submit_button" type="submit" disabled>{{text_content.confirm[lang_nb]}}</button>
+				<button v-else class="submit_button" type="submit">{{text_content.confirm[lang_nb]}}</button>
 			</div>
 		</form>
 	</div>
@@ -68,6 +121,5 @@ export default {
 	margin: 0px;
 	top: 300px;
 }
-
 
 </style>
--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -3,12 +3,12 @@ module.exports = (db_pool) => {
     const auth_middlewares = require("../middlewares/auth.middleware")
     var router = require("express").Router();
 
-    router.post("/signup"          , auth_controller.signup    );
-    router.post("/signin"          , auth_controller.signin    );
-    router.post("/request_pass_reset"          , auth_controller.request_reset_pass    );
-    router.post("/resetpass"       , auth_controller.reset_pass);
+    router.post("/signup"             , auth_controller.signup             );
+    router.post("/signin"             , auth_controller.signin             );
+    router.post("/request_pass_reset" , auth_controller.request_reset_pass );
+    router.post("/resetpass"          , auth_controller.reset_pass         );
 
-    router.get ("/getid"           , auth_middlewares.authenticateToken  , auth_controller.print_id)
+    router.get ("/getid"              , auth_middlewares.authenticateToken , auth_controller.print_id)
 
     return router
 }
